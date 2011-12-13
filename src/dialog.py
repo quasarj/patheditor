bc43ada064a86fbd6b5b0f708f06c103c25926a4
Quasar Jarosz <quasar@ja.rosz.org>
2011-12-13 16:46:35 -0600

    changed from QDialog to QMainWindow

--- src/dialog.py
+++ src/dialog.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\dialog.ui'
+# Form implementation generated from reading ui file 'dialog.ui'
 #
-# Created: Tue Dec 13 15:44:52 2011
+# Created: Tue Dec 13 16:44:51 2011
 #      by: pyside-uic 0.2.8 running on PySide 1.0.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -10,22 +10,21 @@
 from PySide import QtCore, QtGui
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(682, 518)
-        self.widget = QtGui.QWidget(Dialog)
-        self.widget.setGeometry(QtCore.QRect(10, 10, 533, 383))
-        self.widget.setObjectName("widget")
-        self.verticalLayout_2 = QtGui.QVBoxLayout(self.widget)
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setGeometry(QtCore.QRect(20, 20, 682, 518))
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setGeometry(QtCore.QRect(10, 10, 533, 383))
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.centralwidget)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pathList = QtGui.QListWidget(self.widget)
+        self.pathList = QtGui.QListWidget(self.centralwidget)
         self.pathList.setMinimumSize(QtCore.QSize(251, 241))
         self.pathList.setObjectName("pathList")
         self.horizontalLayout.addWidget(self.pathList)
-        self.frame_2 = QtGui.QFrame(self.widget)
+        self.frame_2 = QtGui.QFrame(self.centralwidget)
         self.frame_2.setMinimumSize(QtCore.QSize(91, 271))
         self.frame_2.setMaximumSize(QtCore.QSize(91, 16777215))
         self.frame_2.setFrameShape(QtGui.QFrame.StyledPanel)
@@ -47,12 +46,12 @@ class Ui_Dialog(object):
         self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.lineEdit = QtGui.QLineEdit(self.widget)
+        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
         self.lineEdit.setMinimumSize(QtCore.QSize(0, 22))
         self.lineEdit.setMaximumSize(QtCore.QSize(16777215, 22))
         self.lineEdit.setObjectName("lineEdit")
         self.verticalLayout.addWidget(self.lineEdit)
-        self.frame = QtGui.QFrame(self.widget)
+        self.frame = QtGui.QFrame(self.centralwidget)
         self.frame.setMinimumSize(QtCore.QSize(529, 70))
         self.frame.setMaximumSize(QtCore.QSize(529, 70))
         self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
@@ -68,18 +67,19 @@ class Ui_Dialog(object):
         self.buttonBox.setObjectName("buttonBox")
         self.verticalLayout.addWidget(self.frame)
         self.verticalLayout_2.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(Dialog)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
-        QtCore.QObject.connect(self.pathList, QtCore.SIGNAL("itemClicked(QListWidgetItem*)"), Dialog.itemclick)
-        QtCore.QObject.connect(self.cmdAdd, QtCore.SIGNAL("clicked()"), Dialog.additem)
-        QtCore.QObject.connect(self.cmdDelete, QtCore.SIGNAL("clicked()"), Dialog.removeitem)
-        QtCore.QObject.connect(self.cmdSave, QtCore.SIGNAL("clicked()"), Dialog.saveitem)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), MainWindow.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), MainWindow.reject)
+        QtCore.QObject.connect(self.pathList, QtCore.SIGNAL("itemClicked(QListWidgetItem*)"), MainWindow.itemclick)
+        QtCore.QObject.connect(self.cmdAdd, QtCore.SIGNAL("clicked()"), MainWindow.additem)
+        QtCore.QObject.connect(self.cmdDelete, QtCore.SIGNAL("clicked()"), MainWindow.removeitem)
+        QtCore.QObject.connect(self.cmdSave, QtCore.SIGNAL("clicked()"), MainWindow.saveitem)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Path Edit", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("Dialog", "Path Edit", None, QtGui.QApplication.UnicodeUTF8))
         self.cmdDown.setText(QtGui.QApplication.translate("Dialog", "▼", None, QtGui.QApplication.UnicodeUTF8))
         self.cmdUp.setText(QtGui.QApplication.translate("Dialog", "▲", None, QtGui.QApplication.UnicodeUTF8))
         self.cmdDelete.setText(QtGui.QApplication.translate("Dialog", "Delete", None, QtGui.QApplication.UnicodeUTF8))
